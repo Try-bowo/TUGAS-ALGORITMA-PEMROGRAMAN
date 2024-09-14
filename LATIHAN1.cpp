33ef7d719e906801b986c3f4c5b5a4b1b9387cd1
Try-bowo <bowotri339@gmail.com>
2024-09-14 12:09:08 +0700

    GAJI PEGAWAI

---
+++ LATIHAN1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float nilai1, nilai2, total_nilai;
+    
+    cout << "masukin nilai1 = ";
+    cin >> nilai1;
+    
+    cout << "masukin nilai2 = ";
+    cin >> nilai2;
+    
+    total_nilai = nilai1 + nilai2;
+    
+    cout << "nilai1 + nilai2 = " << total_nilai;
+    
+    return 0;
+}
